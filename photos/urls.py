1a23425524bb9168f9ab8065d98b07d6ad7e11ef
Alex <alex@revotek.com>
2012-12-11 23:34:18 -0800

    Removed author

--- photos/urls.py
+++ photos/urls.py
@@ -1,10 +1,11 @@
-__author__ = 'Alex'
-
 from django.conf.urls import *
 from photos.views import *
+
+
 
 urlpatterns = patterns('',
     url(r'^$', index),
     url(r'^album/(?P<album_id>\d+)$', album),
 
 )
+
