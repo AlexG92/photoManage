e1c374345ffd4d5cf967ad420a1dea75ef1ef16a
Alex <alex@revotek.com>
2012-12-11 23:34:50 -0800

    Added @uri decorator

--- photos/util.py
+++ photos/util.py
@@ -1,1 +1,34 @@
-__author__ = 'Alex-IBuyPower'
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.conf import settings
+from django.conf.urls import url
+from urls import *
+
+def uri(pattern, method='GET', enabled=True, cache={}):
+
+    # Normalize pattern
+    pattern = pattern.lstrip('^').lstrip('/').rstrip('$').rstrip('/')
+    if pattern:
+        pattern = '%s/' % pattern
+    pattern = '^%s$' % pattern
+
+
+    def resolver(request, *args, **kwargs):
+
+        view = cache[pattern].get(request.method)
+        if view:
+            return view(request, *args, **kwargs)
+        else:
+            return HttpResponseNotFound()
+
+    def wrapper(view):
+
+        if enabled:
+            if pattern in cache:
+                cache[pattern][method] = view
+            else:
+                cache[pattern] = {method: view}
+                urlpatterns.append(url(pattern, resolver))
+
+        return view
+
+    return wrapper
