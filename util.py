811ec8d427e30b9336dcc16c24ef5c95bbec123a
Alex <alex@revotek.com>
2012-12-13 14:16:07 -0800

    Added Render Decorator

--- util.py
+++ util.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.conf import settings
 from django.conf.urls import url
 from photoManage.urls import urlpatterns
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def uri(pattern, method='GET', enabled=True, cache={}):
 
@@ -29,6 +31,24 @@ def uri(pattern, method='GET', enabled=True, cache={}):
             else:
                 cache[pattern] = {method: view}
                 urlpatterns.append(url(pattern, resolver))
+
+        return view
+
+    return wrapper
+
+def render(template):
+    '''Specify a template to render a view to'''
+
+    def wrapper(method):
+
+        @wraps(method)
+        def view(request, *args, **kwargs):
+
+            context = method(request, *args, **kwargs)
+            if isinstance(context, HttpResponse):
+                return context
+
+            return render_to_response(template, context, context_instance=RequestContext(request))
 
         return view
 
