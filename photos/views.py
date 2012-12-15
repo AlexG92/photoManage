6893d5e979bc53bd77cc1409216886d0c37955bf
Alex <alex@revotek.com>
2012-12-14 21:15:32 -0800

    Support Custom file placement

--- photos/views.py
+++ photos/views.py
@@ -13,6 +13,8 @@ from photoManage.photos.models import *
 from django.contrib import messages
 from photoManage.util import uri
 from photoManage.util import render
+from photoManage.photos.photofilesystem import get_directory
+import shutil
 
 @uri('photos/')
 @login_required
@@ -50,32 +52,50 @@ def index(request):
         },
         context_instance=RequestContext(request))
 
+@uri('photos/album/(?P<album_id>\d+)')
+@login_required
+def album(request, album_id):
+    albumSelected = get_object_or_404(Album, pk=album_id)
+    photos = Photo.objects.filter(owner = request.user).exclude(album__isnull=False)
+    albumPhotos = Photo.objects.filter(owner = request.user, album_id = albumSelected)
+    return render_to_response('album/index.html',
+        {
+            'photos': photos,
+            'albumSelected': albumSelected,
+            'albumPhotos':albumPhotos,
+            'album':albumSelected,
+        },
+        context_instance=RequestContext(request))
+
 @uri('photos/newphoto/', method='POST')
 @login_required
 @require_POST
 def newphoto(request):
     form = UploadFileForm(request.POST, request.FILES)
+    requestOrigin = request.META['HTTP_REFERER']
     if form.is_valid():
+        albumSelected = request.POST.get('albumid', None)
         for file in request.FILES.getlist('file'):
-            Photo(owner = request.user, pub_date = timezone.now(), photo = file, title = file.name ).save()
-        return HttpResponseRedirect("/photos/")
+            Photo(owner = request.user, pub_date = timezone.now(), photo = file, title = file.name, album_id = albumSelected ).save()
+        return HttpResponseRedirect(requestOrigin)
     else:
         messages.error(request, 'Please upload JPGS, GIFS, or PNG format only.')
-        return HttpResponseRedirect("/photos/")
+        return HttpResponseRedirect(requestOrigin)
 
 @uri('photos/changephotoname/', method='POST')
 @login_required
 @require_POST
 def changephotoname(request):
     form = ChangeFileName(request.POST)
+    requestOrigin = request.META['HTTP_REFERER']
     if form.is_valid():
         photo = Photo.objects.get(pk = request.POST['photoTitleChangeID'], owner = request.user)
         photo.title = request.POST['title']
         photo.save()
-        return HttpResponseRedirect("/photos/")
+        return HttpResponseRedirect(requestOrigin)
     else:
         messages.error(request, 'Please input a proper title')
-        return HttpResponseRedirect("/photos/")
+        return HttpResponseRedirect(requestOrigin)
 
 @uri('photos/deletephoto/', method='POST')
 @login_required
@@ -84,64 +104,87 @@ def deletephoto(request):
     photo = Photo.objects.get(pk = request.POST['delete'], owner = request.user)
     os.remove(photo.photo.path)
     photo.delete()
-    return HttpResponseRedirect("/photos/")
+    requestOrigin = request.META['HTTP_REFERER']
+    return HttpResponseRedirect(requestOrigin)
 
 @uri('photos/newalbum/', method='POST')
 @login_required
 @require_POST
 def newalbum(request):
     form = AlbumForm(request.POST)
+    requestOrigin = request.META['HTTP_REFERER']
     if form.is_valid():
         Album(owner = request.user, title = request.POST['title']).save()
-        return HttpResponseRedirect("/photos/")
+        return HttpResponseRedirect(requestOrigin)
     else:
         messages.error(request, 'Please input a proper title')
-        return HttpResponseRedirect("/photos/")
+        return HttpResponseRedirect(requestOrigin)
 
+##TODO CHANGE ALBUM NAME NOT MOVING FILE DIRECTORY
 @uri('photos/changealbumname/', method='POST')
 @login_required
 @require_POST
 def changealbumname(request):
     form = AlbumForm(request.POST)
+    requestOrigin = request.META['HTTP_REFERER']
     if form.is_valid():
-        album = Album.objects.get(pk = request.POST['albumTitleChangeID'], owner=request.user)
-        album.title = request.POST['title']
-        album.save()
-        return HttpResponseRedirect("/photos/")
+        try:
+            album = Album.objects.get(pk = request.POST['albumTitleChangeID'], owner=request.user)
+            currentAlbumTitle = album.title
+            album.title = request.POST['title']
+            source = get_directory(request.user, None, currentAlbumTitle)
+            destination = get_directory(request.user, None, album.title)
+            print source
+            print destination
+            os.rename(source, destination)
+            photos = Photo.objects.filter(album = album, owner = request.user)
+            for photo in photos:
+                filename = photo.filename()
+                photo.photo = 'photographs/' + photo.owner.username + '/' + album.title + '/' + filename
+                photo.save()
+            album.save()
+            return HttpResponseRedirect(requestOrigin)
+        except Exception as e:
+            messages.error(request, '%s (%s)' % (e.message, type(e)))
+            return HttpResponseRedirect(requestOrigin)
     else:
         messages.error(request, 'Please input a proper title')
-        return HttpResponseRedirect("/photos/")
+        return HttpResponseRedirect(requestOrigin)
 
 @uri('photos/deletealbum/', method='POST')
 @login_required
 @require_POST
 def deletealbum(request):
-    Album.objects.get(pk = request.POST['delete'], owner = request.user).delete()
-    return HttpResponseRedirect("/photos/")
+    requestOrigin = request.META['HTTP_REFERER']
+    album = Album.objects.get(pk = request.POST['delete'], owner = request.user)
+    photos = Photo.objects.filter(album = album, owner = request.user)
+    for photo in photos:
+        filename = photo.filename()
+        source = photo.photo.path
+        destination = get_directory(request.user, filename,)
+        photo.photo = 'photographs/' + photo.owner.username + '/' + filename
+        photo.save()
+        shutil.move(source, destination)
+    currentLocation = get_directory(request.user, None, album.title)
+    try:
+        shutil.rmtree(currentLocation)
+    except Exception as e:
+        messages.error(request, '%s (%s)' % (e.message, type(e)))
+    album.delete()
+    return HttpResponseRedirect(requestOrigin)
 
-@uri('photos/album/(?P<album_id>\d+)')
+@uri('photos/assignphototoalbum/', method='POST')
 @login_required
-def album(request, album_id):
-    albumSelected = get_object_or_404(Album, pk=album_id)
-    photos = Photo.objects.filter(owner = request.user).exclude(album__isnull=False)
-    albumPhotos = Photo.objects.filter(owner = request.user, album_id = albumSelected)
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            for file in request.FILES.getlist('file'):
-                Photo(owner = request.user, pub_date = timezone.now(), photo = file, title = file.name ).save()
-            return render_to_response('album/index.html', {'form': form, 'albumNumber': albumSelected, 'photos': photos, 'albumPhotos':albumPhotos},context_instance=RequestContext(request))
-        elif 'delete' in request.POST:
-            if photo.owner == request.user:
-                photo = Photo(pk = request.POST['delete'])
-                photo.delete()
-            return render_to_response('album/index.html', {'albumNumber': albumSelected, 'photos': photos, 'albumPhotos':albumPhotos},context_instance=RequestContext(request))
-        elif 'addAlbum' in request.POST:
-            photo = Photo.objects.filter(pk = request.POST['addAlbum']).update(album = albumSelected)
-            return render_to_response('album/index.html', {'albumNumber': albumSelected, 'photos': photos, 'albumPhotos':albumPhotos},context_instance=RequestContext(request))
-        else:
-            error_message = "Please input a proper album name"
-            form = AlbumForm()
-            return render_to_response('album/index.html', {'albumNumber': albumSelected, 'error_message': error_message, 'albumPhotos':albumPhotos},context_instance=RequestContext(request))
-    else:
-        return render_to_response('album/index.html', {'photos': photos, 'albumSelected': albumSelected, 'albumPhotos':albumPhotos},context_instance=RequestContext(request))
+@require_POST
+def assignphototoalbum(request):
+    albumSelected = Album.objects.get(pk = request.POST['albumid'], owner = request.user)
+    photo = Photo.objects.get(pk = request.POST['addToAlbum'], owner = request.user)
+    filename = photo.filename()
+    source = photo.photo.path
+    destination = get_directory(request.user, filename, albumSelected)
+    shutil.move(source, destination)
+    photo.album = albumSelected
+    photo.photo = 'photographs/' + photo.owner.username + '/' + albumSelected.title + '/' + filename
+    photo.save()
+    requestOrigin = request.META['HTTP_REFERER']
+    return HttpResponseRedirect(requestOrigin)
