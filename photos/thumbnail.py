b361482ecfdf205b2b8453c560deef3e34295797
Alex <alex@revotek.com>
2012-12-18 21:45:48 -0800

    improved thumb sizing and croping

--- photos/thumbnail.py
+++ photos/thumbnail.py
@@ -1,6 +1,7 @@
 import os
 import Image
 from photofilesystem import get_directory
+from PIL import Image, ImageChops, ImageOps
 
 def create_thumb(owner, filename, album=None):
     photoLocation = get_directory(owner, filename, album)
@@ -8,8 +9,10 @@ def create_thumb(owner, filename, album=None):
     if not os.path.exists(saveLocation):
         os.makedirs(saveLocation)
     photo = Image.open(photoLocation)
-    photo.thumbnail((189,189), Image.ANTIALIAS)
-    photo.save(saveLocation + filename)
+    photo.thumbnail((260,260), Image.ANTIALIAS)
+    thumb = ImageOps.fit(photo, (260,260), Image.ANTIALIAS, (0.5, 0.5))
+    thumb.save(saveLocation + filename)
+    return os.path.join('photographs\\', '%s' % owner, 'thumbnails\\', '%s' % filename)
 
 #from photos.thumbnail import *
 #create_thumb('alex4', 'disk_green_3.png')
